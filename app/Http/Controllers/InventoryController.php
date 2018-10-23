fbc67b70373e3ed15f1965b59719f2c07d98ccad
haisham <haisham42@hotmail.com>
2018-10-23 20:57:14 +0200

    db

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Inventory controller
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Inventory;
+use Illuminate\Support\Facades\Input;
+
+
+class InventoryController extends Controller
+{
+    public function index()
+    {
+        // TODO: get all inventories
+    }
+    /**
+     * Handles input operations on inventory model
+     * @access public
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(Request $reques) {
+
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/inventory', 'InventoryController@store')->name('store'); 
