463d65dbd9c58e59ee9199921d292b18c721c61e
haisham <haisham42@hotmail.com>
2018-10-24 15:29:52 +0200

    controller-update

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Inventory;
+use Validator;
 use Illuminate\Support\Facades\Input;
 
 
@@ -18,13 +19,31 @@ class InventoryController extends Controller
     }
     /**
      * Handles input operations on inventory model
+     *
      * @access public
      * @return \Illuminate\Http\Response
      */
 
-    public function store(Request $reques) {
+    public function store(Request $request)
+    {
+        $date = $request->get('date');
+        $action = $request->get('action');
+        $quantity = $request->get('quantity');
+        $unitPrice = $request->get('unitPrice');
+       
+        $Inventory = new Inventory();
+        $Inventory->action = $action;
+        $Inventory->quantity = $quantity;
+        $Inventory->unit_price = $unitPrice;
+        $Inventory->date = date("Y-m-d H:i:s",strtotime($date));
 
-
+        if($Inventory->save()) {
+            $response = ['success'=>true];
+            return response()->json($response);
+        }else{
+            $response = ['success'=>false];
+            return response()->json($response);
+        }
     }
 
 }
